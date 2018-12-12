564cc99af01efb8383b40ea9379f413077d2f386
alf maglalang <alf@akademe.net>
2018-12-12 21:02:04 +0700

    Conform python code to PEP8 standards

--- newsapp.py
+++ newsapp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# 
+#
 # A web service to expose the news database.
 
 from flask import Flask, request, redirect, url_for
@@ -40,45 +40,51 @@ REPORT2 = '''\
     <div>%s -- %s percent error</div>
 '''
 
+
 def get_topArticles():
-  """Return top three articles from news."""
-  db = psycopg2.connect(database="news")
-  c = db.cursor()
-  c.execute("select title, total from top3articles")  
-  posts = c.fetchall()
-  db.close()
-  return posts
+    """Return top three articles from news."""
+    db = psycopg2.connect(database="news")
+    c = db.cursor()
+    c.execute("select title, total from top3articles")
+    posts = c.fetchall()
+    db.close()
+    return posts
+
 
 def get_popularAuthors():
-  """Return all authors sorted by popularity."""
-  db = psycopg2.connect(database="news")
-  c = db.cursor()
-  c.execute("select name, total from authorsbypopularity")
-  posts = c.fetchall()
-  db.close()
-  return posts
+    """Return all authors sorted by popularity."""
+    db = psycopg2.connect(database="news")
+    c = db.cursor()
+    c.execute("select name, total from authorsbypopularity")
+    posts = c.fetchall()
+    db.close()
+    return posts
+
 
 def get_topDateError():
-  """Return the top date on which more than 1% of requests led to errors."""
-  db = psycopg2.connect(database="news")
-  c = db.cursor()
-  c.execute("select TO_CHAR(date :: DATE, 'Mon dd, yyyy'), error from topdateinerror")
-  posts = c.fetchall()
-  db.close()
-  return posts
+    """Return the top date on which more than 1% of requests led to errors."""
+    db = psycopg2.connect(database="news")
+    c = db.cursor()
+    c.execute(
+        "select TO_CHAR(date::DATE,'Mon dd, yyyy'),error from topdateinerror"
+        )
+    posts = c.fetchall()
+    db.close()
+    return posts
+
 
 @app.route('/', methods=['GET'])
 def main():
-  '''Main page of the reporting tool.'''
-  posts = "<b>1. What are the most popular three articles of all time?</b>"
-  posts += "".join(REPORT % (x, y) for x, y in get_topArticles())
-  posts += "<br><hr><b>2. Who are the most popular article authors of all time?</b>"
-  posts += "".join(REPORT % (x, y) for x, y in get_popularAuthors())
-  posts += "<br><hr><b>3. On which days did more than 1% of requests lead to errors?</b>"
-  posts += "".join(REPORT2 % (x, y) for x, y in get_topDateError())
-  html = HTML_WRAP % posts
-  return html
+    '''Main page of the reporting tool.'''
+    posts = "<b>1. What are the most popular three articles of all time?</b>"
+    posts += "".join(REPORT % (x, y) for x, y in get_topArticles())
+    posts += "<br><hr><b>2. Who are the most popular authors of all time?</b>"
+    posts += "".join(REPORT % (x, y) for x, y in get_popularAuthors())
+    posts += "<br><hr><b>3. What days led to more 1% of error requests?</b>"
+    posts += "".join(REPORT2 % (x, y) for x, y in get_topDateError())
+    html = HTML_WRAP % posts
+    return html
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
