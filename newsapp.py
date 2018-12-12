cf4d517f9ee0c3faae4bdfe715f9e5c541b160e7
alf maglalang <alf@akademe.net>
2018-12-12 19:37:57 +0700

    Have a running news reporting application

--- newsapp.py
+++ newsapp.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # 
-# A buggy web service in need of a database.
+# A web service to expose the news database.
 
 from flask import Flask, request, redirect, url_for
 
-from newsappdb import get_articles
+import psycopg2
 
 app = Flask(__name__)
 
-# HTML template for the forum page
+# HTML template for the reporting tool page
 HTML_WRAP = '''\
 <!DOCTYPE html>
 <html>
@@ -17,16 +17,12 @@ HTML_WRAP = '''\
     <style>
       h1, form { text-align: center; }
       textarea { width: 400px; height: 100px; }
-      div.post { border: 1px solid #999;
-                 padding: 10px 10px;
-                 margin: 10px 20%%; }
       hr.postbound { width: 50%%; }
-      em.date { color: #999 }
     </style>
   </head>
   <body>
-    <h1>News Data Report</h1>
-
+    <h1>News Database Reporting Tool</h1>
+    <h3>The answers to udaciously important questions about news DB.</h3>
     <!-- report content will go here -->
 %s
   </body>
@@ -35,24 +31,53 @@ HTML_WRAP = '''\
 
 # HTML template for listing items
 REPORT = '''\
-    <div class=post><em class=date>%s</em><br>%s</div>
+    <table><tr>
+    <td>%s</td><td> -- </td><td>%s views</td>
+    </tr></table>
 '''
 
+REPORT2 = '''\
+    <div>%s -- %s percent error</div>
+'''
+
+def get_topArticles():
+  """Return top three articles from news."""
+  db = psycopg2.connect(database="news")
+  c = db.cursor()
+  c.execute("select title, total from top3articles")  
+  posts = c.fetchall()
+  db.close()
+  return posts
+
+def get_popularAuthors():
+  """Return all authors sorted by popularity."""
+  db = psycopg2.connect(database="news")
+  c = db.cursor()
+  c.execute("select name, total from authorsbypopularity")
+  posts = c.fetchall()
+  db.close()
+  return posts
+
+def get_topDateError():
+  """Return the top date on which more than 1% of requests led to errors."""
+  db = psycopg2.connect(database="news")
+  c = db.cursor()
+  c.execute("select TO_CHAR(date :: DATE, 'Mon dd, yyyy'), error from topdateinerror")
+  posts = c.fetchall()
+  db.close()
+  return posts
 
 @app.route('/', methods=['GET'])
 def main():
-  '''Main page of the forum.'''
-  posts = "".join(REPORT % (date, text) for text, date in get_articles())
+  '''Main page of the reporting tool.'''
+  posts = "<b>1. What are the most popular three articles of all time?</b>"
+  posts += "".join(REPORT % (x, y) for x, y in get_topArticles())
+  posts += "<br><hr><b>2. Who are the most popular article authors of all time?</b>"
+  posts += "".join(REPORT % (x, y) for x, y in get_popularAuthors())
+  posts += "<br><hr><b>3. On which days did more than 1% of requests lead to errors?</b>"
+  posts += "".join(REPORT2 % (x, y) for x, y in get_topDateError())
   html = HTML_WRAP % posts
   return html
-
-
-@app.route('/', methods=['POST'])
-def post():
-  '''New post submission.'''
-  message = request.form['content']
-  add_post(message)
-  return redirect(url_for('main'))
 
 
 if __name__ == '__main__':
--- views_created.sql
+++ views_created.sql
@@ -2,44 +2,29 @@
 -- It will facilitate the join with articles since there are no common keys.
 -- After the join on path and slug, the slug will be the unique identifier.
 -- See view lpwc_articles below.
-
 create view logPathWithCount as select path, count(*) as num from log group by path;
 
 
-select logPathWithCount.*, articles.slug, articles.title, articles.author as author_id from logPathWithCount join articles on logPathWithCount.path ~ articles.slug order by logPathWithCount.num desc;
-
 -- This presents a join of logPathWithCount and articles on path and slug.
 -- This was facilitated by the view logPathWithCount above
-
 create view lpwc_articles as select lpc.*, a.slug, a.title, a.author as author_id from logPathWithCount as lpc join articles as a on lpc.path ~ a.slug order by lpc.num desc;
-
-select DISTINCT la.slug, la.title, s.total from lpwc_articles as la join (select slug, SUM(num) as total from lpwc_articles group by slug) as s on la.slug = s.slug order by s.total desc;
 
 
 -- This presents all the dates with error status (or NOT '200 OK') with count.
 -- Along with view requestsPerDate below, this facilitates presentation of proportion error to total.
 create view datesWithErrorStatus as select DATE(time) as date, count(*) as num from log where status != '200 OK' group by date order by date desc;
 
-select *, round((num * 100.0) / 1677735, 2) as error from dateswitherrorstatus;
-
 
 -- This presents the total requests by date.
 -- Along with view datesWithErrorStatus above, this facilitates presentation of proportion error to total.
-
 create view requestsPerDate as select DATE(time) as date, count(*) as num from log group by date order by date desc;
 
 
-select dwes.*, rpd.num as total_requests, round((dwes.num * 100.0) / rpd.num, 2) as error from datesWithErrorStatus as dwes join requestsPerDate as rpd on dwes.date = rpd.date where error > 1;
-
-
 -- This presents the query to select top 3 articles.
-
 create view top3articles as select DISTINCT la.slug, la.title, s.total from lpwc_articles as la join (select slug, SUM(num) as total from lpwc_articles group by slug) as s on la.slug = s.slug order by s.total desc limit 3;
 
 -- This presents authors ordered by popularity.
-
 create view authorsbypopularity as select authors.name, sss.total from authors join (select DISTINCT la.author_id, s.total from lpwc_articles as la join (select author_id, SUM(num) as total from lpwc_articles group by author_id) as s on la.author_id = s.author_id order by s.total desc) as sss on authors.id = sss.author_id;
 
 -- This presents all dates that have percentage error of greater than 1 percent.
-
 create view topdateinerror as select dwes.date, round((dwes.num * 100.0) / rpd.num, 2) as error from datesWithErrorStatus as dwes join requestsPerDate as rpd on dwes.date = rpd.date where round((dwes.num * 100.0) / rpd.num, 2) > 1;
